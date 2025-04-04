3279980d58924576c30fdbd42fec7d80e8bc93a0
Alfredo Israel Bautista Santamaria <abautista@selder.com.mx>
2025-04-04 11:06:22 -0600

    Cambios info Selder: Actualización de estilos, scripts y contenido

--- assets/js/SelderScript.js
+++ assets/js/SelderScript.js
@@ -1,15 +1,11 @@
 /*!
 =========================================================
-* Creative Design Landing page
+* Selder Landing page
 =========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
+* Copyright: 2025 DevCRUD (https://selder.com)
+* Coded by: ABAUTISTA.
 =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */ 
 
 // smooth scroll
